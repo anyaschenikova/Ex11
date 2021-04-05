07a83620d00280b7319beca88e328e2c35e60984
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2021-04-05 22:11:45 +0300

    add tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,7 +1,31 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
 #include <string>
-#include "train.h"
+#include "../include/train.h"
 
+TEST(Train, test1) {
+  Train* train = new Train;
+  for (int i = 0; i < 10; i++) {
+    Cage* cage = new Cage;
+    train->addCage(cage);
+  }
+  EXPECT_EQ(train->counter(), 10);
+}
 
+TEST(Train, test2) {
+  Train* train = new Train;
+  for (int i = 0; i < 5; i++) {
+    Cage* cage = new Cage;
+    train->addCage(cage);
+  }
+  EXPECT_EQ(train->counter(), 5);
+}
 
+TEST(Train, test3) {
+  Train* train = new Train;
+  for (int i = 0; i < 20; i++) {
+    Cage* cage = new Cage;
+    train->addCage(cage);
+  }
+  EXPECT_EQ(train->counter(), 20);
+}
